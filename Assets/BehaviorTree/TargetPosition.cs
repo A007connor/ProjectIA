a7c619a869b0c4ba4d47be209d00da3e6aa53407
GaetanPe <gaetan.peronnet@gmail.com>
2023-12-19 14:27:06 +0100

    update behavior tree

--- Assets/BehaviorTree/TargetPosition.cs
+++ Assets/BehaviorTree/TargetPosition.cs
@@ -6,12 +6,12 @@ using TheKiwiCoder;
 public class TargetPosition : ActionNode
 {
     DataEnemies dataEnemies;
-    Vector3 min = Vector3.one;
-    Vector3 max = Vector3.one;
+    Vector3 min = Vector3.one * 2;
+    Vector3 max = Vector3.one * 2;
     protected override void OnStart() {
         dataEnemies = context.gameObject.GetComponent<DataEnemies>();
-        dataEnemies.target.x = Random.Range(min.x, max.x);
-        dataEnemies.target.y = Random.Range(-min.y, max.y);
+        dataEnemies.target.x = dataEnemies._spawn.position.x + Random.Range(min.x, max.x);
+        dataEnemies.target.y = dataEnemies._spawn.position.y + Random.Range(-min.y, max.y);
     }
 
     protected override void OnStop() {
