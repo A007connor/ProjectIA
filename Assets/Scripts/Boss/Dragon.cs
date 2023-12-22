2f3ae52e8053f4b538cd13cac7f216def18b4b03
jantoine98 <antoinejarnoux@hotmail.com>
2023-12-22 11:59:07 +0100

    Update dragon movement + FireBall

--- Assets/Scripts/Boss/Dragon.cs
+++ Assets/Scripts/Boss/Dragon.cs
@@ -4,15 +4,87 @@ using UnityEngine;
 
 public class Dragon : MonoBehaviour
 {
+
+    [SerializeField] LayerMask wallLayer;
+    [SerializeField] int maxhp = 8;
+    [SerializeField] int currentHp;
+    [SerializeField] StateManager currentState;
+    [SerializeField] Transform playerTransform;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float maxMoveSpeed;
+    [SerializeField] float chargeSpeed;
+    [SerializeField] float maxChargeSpeed;
+    [SerializeField] float maxCooldown;
+    [SerializeField] float currentcooldown;
+    [SerializeField] ChaseState chaseState;
+    [SerializeField] AttackState attackState;
+    [SerializeField] IdleState idleState;
+    Vector3 chargeDirection;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHp = maxhp;
+        moveSpeed = maxMoveSpeed;
+        chargeSpeed = maxMoveSpeed;
+        currentcooldown = maxCooldown;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        ChangeState();
+
+        if (currentState.GetState() is ChaseState)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, playerTransform.position, moveSpeed * Time.deltaTime);
+            chargeDirection = Vector3.zero;
+        }
+        if (currentState.GetState() is AttackState)
+        {
+            if (chargeDirection == Vector3.zero)
+            {
+                chargeDirection = (playerTransform.position - transform.position).normalized;
+
+            }
+            transform.position = Vector2.MoveTowards(transform.position, transform.position + chargeDirection, chargeSpeed * Time.deltaTime);
+
+        }
+        if (currentcooldown < maxCooldown)
+        {
+            moveSpeed = 0;
+            chargeSpeed = 0;
+            currentcooldown += 1 * Time.deltaTime;
+        }
+        else
+        {
+            moveSpeed = maxMoveSpeed;
+            chargeSpeed = maxChargeSpeed;
+        }
+        if (currentHp == 0)
+        {
+            Death();
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & wallLayer) != 0)
+        {
+            currentHp -= 1;
+            Debug.Log("hurt");
+            currentcooldown = 0;
+        }
+
+    }
+    void Death()
+    {
+        Destroy(gameObject);
+
+
+    }
+    void ChangeState()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, playerTransform.position);
+        if (distanceToPlayer > 10f) currentState.SwitchNextState(idleState);
+        if (distanceToPlayer <= 6f) currentState.SwitchNextState(chaseState);
+        if (distanceToPlayer <= 4f) currentState.SwitchNextState(attackState);
     }
 }
--- Assets/Scripts/StateMachine/ChaseState.cs
+++ Assets/Scripts/StateMachine/ChaseState.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ChaseState : State
 {
@@ -8,10 +9,51 @@ public class ChaseState : State
     [SerializeField] AttackState attackState;
     [SerializeField] bool inRange;
     [SerializeField] bool outRange;
+
+    [SerializeField] NavMeshAgent navMeshAgent;
+    [SerializeField] Transform player; // Référence au joueur
+    [SerializeField] float detectionRange = 10f; // Portée de détection du joueur
+    [SerializeField] float verticalSpeed = 2f; // Vitesse de déplacement vertical
+    [SerializeField] GameObject fireballPrefab; // Préfabriqué de boule de feu
+    [SerializeField] Transform firePoint; // Point de spawn des boules de feu
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
     public override State RunCurrentState()
     {
-            
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        // Si le joueur est dans la zone de détection
+        if (distanceToPlayer <= detectionRange)
+        {
+            // Déplacer le dragon vers le joueur (horizontal)
+            navMeshAgent.SetDestination(player.position);
+
+            // Déplacer le dragon vers le haut ou le bas (vertical)
+            float verticalMovement = Mathf.Sin(Time.time) * verticalSpeed;
+
+            // Utiliser le NavMeshAgent pour le mouvement vertical
+            navMeshAgent.Move(Vector3.up * verticalMovement * Time.deltaTime);
+
+            // Lancer une boule de feu
+            LaunchFireball();
+        }
+        else
+        {
+            // Si le joueur n'est pas dans la zone, passer à l'état Idle
+            return idleState;
+        }
+
         return this;
-        
+    }
+
+    private void LaunchFireball()
+    {
+        // Instancier une boule de feu au point de spawn
+        Instantiate(fireballPrefab, firePoint.position, firePoint.rotation);
     }
 }
